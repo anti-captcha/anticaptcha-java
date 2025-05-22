61deeac60ed392da9f3b33dc12df7851d1386041
Anti Admin <noreply@anti-captcha.com>
2025-05-22 08:56:08 +0300

    Amazon WAF support

---
+++ src/com/anti_captcha/Api/AmazonProxyless.java
@@ -0,0 +1,73 @@
+package com.anti_captcha.Api;
+
+import com.anti_captcha.AnticaptchaBase;
+import com.anti_captcha.ApiResponse.TaskResultResponse;
+import com.anti_captcha.Helper.DebugHelper;
+import com.anti_captcha.IAnticaptchaTaskProtocol;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+
+public class AmazonProxyless extends AnticaptchaBase implements IAnticaptchaTaskProtocol {
+    private URL websiteUrl;
+    private String websiteKey;
+    private String iv;
+    private String context;
+    private String captchaScript;
+    private String challengeScript;
+
+    public void setWebsiteUrl(URL websiteUrl) {
+        this.websiteUrl = websiteUrl;
+    }
+
+    public void setWebsiteKey(String websiteKey) {
+        this.websiteKey = websiteKey;
+    }
+
+    public void setIv(String iv) {
+        this.iv = iv;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public void setCaptchaScript(String captchaScript) {
+        this.captchaScript = captchaScript;
+    }
+
+    public void setChallengeScript(String challengeScript) {
+        this.challengeScript = challengeScript;
+    }
+
+    @Override
+    public JSONObject getPostData() {
+        JSONObject postData = new JSONObject();
+
+        try {
+            postData.put("type", "AmazonTaskProxyless");
+            postData.put("websiteURL", websiteUrl.toString());
+            postData.put("websiteKey", websiteKey);
+            postData.put("iv", iv);
+            postData.put("context", context);
+            if (captchaScript != null && !captchaScript.isEmpty()) {
+                postData.put("captchaScript", captchaScript);
+            }
+            if (challengeScript != null && !challengeScript.isEmpty()) {
+                postData.put("challengeScript", challengeScript);
+            }
+        } catch (JSONException e) {
+            DebugHelper.out("JSON compilation error: " + e.getMessage(), DebugHelper.Type.ERROR);
+
+            return null;
+        }
+
+        return postData;
+    }
+
+    @Override
+    public TaskResultResponse.SolutionData getTaskSolution() {
+        return taskInfo.getSolution();
+    }
+}
